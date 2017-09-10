8bf6934c8c075113a478aae83127ff8b880fde62
Kenny <ken12938@yahoo.com>
2017-09-10 05:04:51 -0400

    Integrated summarizer and nexmo

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var Nexmo = require('nexmo');
-
-var junkWords = ["summarize", ];
+var AYLIENTextAPI = require('aylien_textapi');
 
 var nexmo = new Nexmo({
   apiKey: '2aec9001',
   apiSecret: '33373eccbd477bee',
+});
+var textapi = new AYLIENTextAPI({
+    application_id: '39acf72e',
+    application_key: '2f1cbec3166847a1c0e78dc724b25290'
 });
 
 app.get("/", function(req, res) {
@@ -19,25 +22,40 @@ app.get('/incoming-sms', (req, res) => {
   var msisdn = req.query.msisdn;
   var text = req.query.text.toLowerCase();
 
-  var textArray = text.split(' ');
-  var sentences;
-  var newArr = [];
-
-  for(var i = 0; i < textArray.length - 1; i++) {
-    if(textArray[i + 1].includes("sentence")) {
-      sentences = parseInt(textArray[i]);
-    }
-
-    //WORK IN PROGRESS
-  }
+  var textArray = text.split(',');
+  var sentences = parseInt(textArray[1].trim().split(' ')[0]);
+  var message = parseInt(textArray[0].trim());
 
   //var newtext = summarize(text);
 
-  nexmo.message.sendSms('12016441578', msisdn, text, (err, responseData) => {
-    if (err) {
-      console.log(err);
+  textapi.summarize({
+    title: 'Articles',
+    text: 'The Treaties of the European Union are a set of international treaties between the European Union (EU) member states which sets out the EU\'s constitutional basis. They establish the various EU institutions together with their remit, procedures and objectives. The EU can only act within the competences granted to it through these treaties and amendment to the treaties requires the agreement and ratification (according to their national procedures) of every single signatory. Two core functional treaties, the Treaty on European Union (originally signed in Maastricht in 1992) and the Treaty on the Functioning of the European Union (originally signed in Rome in 1957 as the Treaty establishing the European Economic Community), lay out how the EU operates, and there are a number of satellite treaties which are interconnected with them. The treaties have been repeatedly amended by other treaties over the 65 years since they were first signed. The consolidated version of the two core treaties is regularly published by the European Commission.',
+    sentences_number: 2
+  }, function(error, response) {
+    if (error === null) {
+      var summary = '';
+
+      response.sentences.forEach(function(s) {
+        summary = summary.concat(s + '\n');
+      });
+
+      nexmo.message.sendSms('12016441578', msisdn, summary, (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.dir(responseData);
+        }
+      });
     } else {
-      console.dir(responseData);
+      console.log('Reached here');
+      nexmo.message.sendSms('12016441578', msisdn, error, (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.dir(responseData);
+        }
+      });
     }
   });
 
