8945d2e58a069d42d87f5055b15a2c4239cf0593
Kenny <ken12938@yahoo.com>
2017-09-10 05:39:54 -0400

    buggy integration of youtube dl

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 var Nexmo = require('nexmo');
 var AYLIENTextAPI = require('aylien_textapi');
+var request = require('request-promise');
+var fs = require('fs');
+var path = require('path');
+
+//consts
+const execSync = require('child_process').execSync;
+const api_key = "AIzaSyDjwrD8gFTDvtXontSBESw9VqmKm_ealgc";
+const baseURL = "https://www.googleapis.com/youtube/v3";
 
 var nexmo = new Nexmo({
   apiKey: '2aec9001',
@@ -25,39 +33,64 @@ app.get('/incoming-sms', (req, res) => {
   var textArray = text.split(',');
   var sentences = parseInt(textArray[1].trim().split(' ')[0]);
   var message = parseInt(textArray[0].trim());
+  let videoIdPath = "/search?part=snippet&maxResults=1&order=relevance&q="+encodeURIComponent(message)+"&type=video&videoCaption=closedCaption&relevanceLanguage=en&key="+api_key;
 
-  //var newtext = summarize(text);
+  request({
+    "method":"GET",
+    "uri": baseURL+videoIdPath,
+    "json": true
+  }).then(function(response){
+    let vidId = response.items[0].id.videoId;
 
-  textapi.summarize({
-    title: 'Articles',
-    text: 'The Treaties of the European Union are a set of international treaties between the European Union (EU) member states which sets out the EU\'s constitutional basis. They establish the various EU institutions together with their remit, procedures and objectives. The EU can only act within the competences granted to it through these treaties and amendment to the treaties requires the agreement and ratification (according to their national procedures) of every single signatory. Two core functional treaties, the Treaty on European Union (originally signed in Maastricht in 1992) and the Treaty on the Functioning of the European Union (originally signed in Rome in 1957 as the Treaty establishing the European Economic Community), lay out how the EU operates, and there are a number of satellite treaties which are interconnected with them. The treaties have been repeatedly amended by other treaties over the 65 years since they were first signed. The consolidated version of the two core treaties is regularly published by the European Commission.',
-    sentences_number: 2
-  }, function(error, response) {
-    if (error === null) {
-      var summary = '';
+    var script = "youtube-dl --all-subs --skip-download https://www.youtube.com/watch?v="+vidId;
+    var res = execSync(script);
 
-      response.sentences.forEach(function(s) {
-        summary = summary.concat(s + '\n');
-      });
+    var captionTrack;
 
-      nexmo.message.sendSms('12016441578', msisdn, summary, (err, responseData) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.dir(responseData);
-        }
-      });
-    } else {
-      console.log('Reached here');
-      nexmo.message.sendSms('12016441578', msisdn, error, (err, responseData) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.dir(responseData);
-        }
-      });
+    var files = fs.readdirSync('C:/Users/Kenny/Favorites/Documents/Nexmo Project/');
+    for (var i in files) {
+      if(path.extname(files[i]) === ".vtt") {
+        fs.readFile(files[i], 'utf8', function(err, contents) {
+          captionTrack = contents;
+        });
+      }
     }
-  });
+    
+    execSync('del *.vtt');
+
+    console.log(captionTrack);
+    
+    /*textapi.summarize({
+      title: 'Summary',
+      text: captionTrack,
+      sentences_number: 2
+    }, function(error, response) {
+      if (error === null) {
+        var summary = '';
+
+        response.sentences.forEach(function(s) {
+          summary = summary.concat(s + '\n');
+        });
+
+        nexmo.message.sendSms('12016441578', msisdn, summary, (err, responseData) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.dir(responseData);
+          }
+        });
+      } else {
+        console.log('Reached here');
+        nexmo.message.sendSms('12016441578', msisdn, error, (err, responseData) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.dir(responseData);
+          }
+        });
+      }
+    });*/
+  }, console.log);
 
   res.sendStatus(200);
 });
